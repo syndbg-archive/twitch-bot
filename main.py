a89526e26c62d7f1cc6b5f506a6da73876b310aa
syndbg <syndbe@gmail.com>
2015-01-03 15:37:38 +0200

    Added basic bot run

--- main.py
+++ main.py
@@ -1,1 +1,26 @@
 from conf import conf
+from connection import IRC
+
+
+class MisterRoboto:
+
+    def __init__(self, socket):
+        self.socket = socket
+
+    def run(self):
+        self.socket.connect()
+        self.socket.auth()
+
+        while True:
+            response = self.socket.recv(2048).rstrip()
+
+            if len(response) == 0:
+                self.socket = self.socket.connect()
+                self.socket = self.socket.auth()
+
+            self.socket.ping_pong(response)
+
+
+if __name__ == '__main__':
+    irc = IRC(conf)
+    bot = MisterRoboto(irc.sock)
